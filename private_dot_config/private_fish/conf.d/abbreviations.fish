b90768526f5a24911fdcdb13164fe1acf783d76f
Tiago Costa <costa.tiago@gmail.com>
2023-04-26 00:39:02 +0200

    Add .config/fish/conf.d/abbreviations.fish

---
+++ private_dot_config/private_fish/conf.d/abbreviations.fish
@@ -0,0 +1,10 @@
+abbr rm 'trash -v'
+abbr rcp 'rsync -auvPL'
+abbr lg lazygit
+abbr e nvim
+abbr cm chezmoi
+abbr cm-preview 'cm -vn apply'
+abbr ls exa
+abbr la 'll -a'
+abbr cl 'clear && printf "\033[3J"'
+abbr yay-rmf 'yay -Rnscu'

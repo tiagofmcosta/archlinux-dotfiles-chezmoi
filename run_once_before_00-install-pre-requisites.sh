37ff5ba9f815539c9bff0134dc99abdd70093705
Tiago Costa <costa.tiago@gmail.com>
2023-04-25 19:20:27 +0200

    Add age and meld to the pre-requisites

--- run_once_before_00-install-pre-requisites.sh
+++ run_once_before_00-install-pre-requisites.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy --needed --noconfirm fish git curl ksshaskpass rbw pinentry
+sudo pacman -Sy --needed --noconfirm fish git curl ksshaskpass rbw pinentry age meld
 
 rbw config set email costa.tiago@gmail.com
 rbw config set pinentry pinentry-qt
